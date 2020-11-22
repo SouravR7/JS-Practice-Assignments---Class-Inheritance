3a700db1f7087d2b7cf6c1435f48106e7c193156
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-11-22 15:40:34 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
 
 // declare class
+import Shape from "./shape";
 
+// declare class
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+
+  calculateArea() {}
+}
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,9 @@
 // declare class
-
+export default class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape() {}
+  calculateArea() {}
+}
 // export class using module.exports
